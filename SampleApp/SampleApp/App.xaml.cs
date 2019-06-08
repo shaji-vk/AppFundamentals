0f642ec49f648a3529bfafc3355c35d46d1d5910
Shaji Kumar VK <shaji_vk@hotmail.co.in>
2019-06-08 17:04:12 +0530

    Behavior-I

---
+++ SampleApp/SampleApp/App.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Diagnostics;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace SampleApp
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+
+        protected override void OnStart()
+        {
+            Debug.WriteLine("OnStart() - Method");
+        }
+
+        protected override void OnSleep()
+        {
+            Debug.WriteLine("OnSleep() - Method");
+        }
+
+        protected override void OnResume()
+        {
+            Debug.WriteLine("OnResume() - Method");
+        }
+    }
+}
---
+++ XamarinForms.Behaviours/XamarinForms.Behaviours/NumericValidationBehavior.cs
@@ -0,0 +1,24 @@
+﻿using Xamarin.Forms;
+
+namespace XamarinForms.Behaviours
+{
+    public class NumericValidationBehavior : Behavior<Entry>
+    {
+        protected override void OnAttachedTo(Entry bindable)
+        {
+            bindable.TextChanged += OnEntryTextChanged;
+            base.OnAttachedTo(bindable);
+        }
+        protected override void OnDetachingFrom(Entry bindable)
+        {
+            bindable.TextChanged -= OnEntryTextChanged;
+            base.OnDetachingFrom(bindable);
+        }
+        void OnEntryTextChanged(object sender, TextChangedEventArgs args)
+        {
+            double result;
+            bool isValid = double.TryParse(args.NewTextValue, out result);
+            ((Entry)sender).TextColor = isValid ? Color.Default : Color.Red;
+        }
+    }
+}
